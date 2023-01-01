c66e4701d649810af3e5a10e97ade8c9c8b74bb4
Gordon-Magill <gordon.magill@gmail.com>
2022-12-31 23:49:14 -0800

    Improved auth.js middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+// Don't store your secrets in some garbage js! Use environment variables!
+// const secret = 'mysecretsshhhhh';
+const secret = process.env.project_secret
+
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via  req.query or headers (NEW: or with token in the body, even if nonstand it's good to have)
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -28,8 +31,12 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
+    // OLD: next() would be relevant if this were being used in Express, but this is 
     // send to next endpoint
-    next();
+    // next();
+
+    // Return the modified request (now with data extracted from the verification of the token)
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
