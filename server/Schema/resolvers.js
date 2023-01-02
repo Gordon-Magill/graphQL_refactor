e2ccf9b858a328e2a29b826ea3cfe3a581560e03
Gordon-Magill <gordon.magill@gmail.com>
2023-01-02 03:25:41 -0800

    Updated resolvers

--- server/Schema/resolvers.js
+++ server/Schema/resolvers.js
@@ -36,9 +36,55 @@ const resolvers = {
         }
       }
     },
-    addUser: async (parent, args, context, info) => {},
-    saveBook: async (parent, args, context, info) => {},
-    removeBook: async (parent, args, context, info) => {},
+    addUser: async (parent, args, context, info) => {
+      // Args should be defined in typeDefs - in this case can just dump the whole args in since it's named correctly on the other end
+      const user = await User.create(args);
+      // Create a new login token for the created user
+
+      const newToken = signToken(user);
+
+      // Return the equivalent of an Auth object
+      return { token, user };
+    },
+    saveBook: async (parent, args, context, info) => {
+      console.log("saveBook this:", this);
+      console.log("saveBook parent:", parent);
+      console.log("saveBook args:", args);
+      console.log("saveBook context:", context);
+      console.log("saveBook info:", info);
+
+      if (context.user) {
+        const newBookUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { new: true },
+          {
+            $push: {
+              savedBooks: args.bookId,
+            },
+          }
+        );
+        return newBookUser;
+      } else {
+        console.log("saveBook Resolver: No authenticated user in context");
+      }
+    },
+    removeBook: async (parent, args, context, info) => {
+      if (context.user) {
+        const removeBookUser = User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { new: true },
+          {
+            $pull: {
+              savedBooks: args.bookId,
+            },
+          }
+        );
+
+        return removeBookUser;
+      } else {
+        console.log("removeBook Resolver: No authenticated user in context");
+      }
+    },
   },
 };
 
