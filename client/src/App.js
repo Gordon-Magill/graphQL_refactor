1e609501b8c043a08c0ae000b83b236812101f6a
Gordon-Magill <gordon.magill@gmail.com>
2023-01-02 17:18:32 -0800

    Converted client App.js to use ApolloClient and use tokens in authorization header

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
+// Add in new Apollo components
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// Create base endpoint for all graphQL queries
+const gqlLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Create context on each request w/ token info
+const tokenContext = setContext((_, { headers }) => {
+  // Use localstorage if it's available
+  const token = lolcalStorage.getItem("id_token");
+  const tokenContent = token ? `Bearer ${token}` : "";
+
+  // Return modified headers
+  return {
+    ...headers,
+    authorization: tokenContent,
+  };
+});
+
+// Create new ApolloClient for use by ApolloProvider
+const apolloClient = new ApolloClient({
+  link: tokenContext.concat(gqlLink),
+  cache: new InMemoryCache(),
+});
+
+// Wrapping page content w/ ApolloProvider so that all pages get access
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
