3b3327202a18e542b9c2e93af5df4b09c0b263a1
Gordon-Magill <gordon.magill@gmail.com>
2023-01-05 16:44:58 -0800

    Fixed books being added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 // Import the main data types
-const { User } = require("../models");
+const { User } = require("../models/");
 
 // Import the token signing / creation function for use with login functionality
 const { signToken } = require("../utils/auth");
@@ -7,8 +7,8 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context, info) => {
-      console.log('me query resolver activated')
-      console.log("context:",context)
+      console.log("me query resolver activated");
+      // console.log("context:", context);
       // If the authentication middleware didn't attach user info, send an alert
       if (context.user) {
         const user = await User.findOne({ _id: context.user._id }).select(
@@ -22,11 +22,11 @@ const resolvers = {
   },
   Mutation: {
     login: async (parent, args, context, info) => {
-      console.log('login server mutation resolver triggered')
-      console.log(args)
+      console.log("login server mutation resolver triggered");
+      console.log(args);
       // Find any users that have the same email submitted in the login attempt
       const user = await User.findOne({ email: args.email });
-      console.log(`user:`, user)
+      console.log(`user:`, user);
       if (!user) {
         console.log("Login mutation resolver error: incorrect user email");
       } else {
@@ -37,46 +37,48 @@ const resolvers = {
         } else {
           // Provide a an Auth object for the valid user
           const token = signToken(user);
-          console.log({token, user})
+          console.log({ token, user });
           return { token, user };
         }
       }
     },
 
     addUser: async (parent, args, context, info) => {
-      console.log('addUser mutation resolver activated on server')
-      console.log(`addUser args:`, args)
+      console.log("addUser mutation resolver activated on server");
+      console.log(`addUser args:`, args);
       // Args should be defined in typeDefs - in this case can just dump the whole args in since it's named correctly on the other end
       const user = await User.create(args);
-    
+
       // Create a new login token for the created user
 
       const token = signToken(user);
 
       // Return the equivalent of an Auth object
-      console.log('addUser mutation resolver completed on server')
-      console.log('token:', token)
-      console.log('user:', user)
+      console.log("addUser mutation resolver completed on server");
+      console.log("token:", token);
+      console.log("user:", user);
       return { token, user };
     },
 
     saveBook: async (parent, args, context, info) => {
-      console.log("saveBook this:", this);
-      console.log("saveBook parent:", parent);
-      console.log("saveBook args:", args);
-      console.log("saveBook context:", context);
-      console.log("saveBook info:", info);
+      console.log("saveBook mutation resolver activated");
+      // console.log("saveBook this:", this);
+      // console.log("saveBook parent:", parent);
+      console.log("saveBook args.bookInfo:", args.bookInfo);
+      console.log("saveBook context.user._id:", context.user._id);
+      // console.log("saveBook info:", info);
 
       if (context.user) {
         const newBookUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { new: true },
           {
             $push: {
-              savedBooks: args.bookId,
+              savedBooks: args.bookInfo,
             },
-          }
+          },
+          { new: true },
         );
+        console.log('newBookUser:', newBookUser)
         return newBookUser;
       } else {
         console.log("saveBook Resolver: No authenticated user in context");
